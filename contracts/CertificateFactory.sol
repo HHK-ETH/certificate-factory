d10299110970300651e75372cd71e2755904c3fa
HHK <hhk.contact@protonmail.com>
2021-06-07 17:12:31 +0200

    feat: add events & remove certificateHashes array

--- contracts/CertificateFactory.sol
+++ contracts/CertificateFactory.sol
@@ -8,14 +8,18 @@ contract CertificateFactory is Ownable {
 
     //hash => ipfs hash
     mapping (string => string) public certificates;
-    string[] public certificateHashes;
+
+    event logAddCertificate(string indexed hash, string ipfsHash);
+    event logRemoveCertificate(string indexed hash);
 
     function addCertificate(string calldata _hash, string calldata _ipfsHash) onlyOwner() external {
-        require(bytes(certificates[_hash]).length == 0, 'This hash already exist');
+        require(bytes(certificates[_hash]).length == 0, 'CertificateFactory: This hash already exist');
         certificates[_hash] = _ipfsHash;
+        emit logAddCertificate(_hash, _ipfsHash);
     }
 
     function removeCertificate(string calldata _hash) onlyOwner() external {
         delete certificates[_hash];
+        emit logRemoveCertificate(_hash);
     }
 }
