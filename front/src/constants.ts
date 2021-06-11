6d8f1150415877a0630609155d8d1ac7416de93d
HHK <hhk.contact@protonmail.com>
2021-06-11 16:06:46 +0200

    feat: add aws connector to upload file on ipfs

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -134,3 +134,14 @@ export const FACTORY_ABI = [
         "type": "function"
     }
 ];
+
+const AWS = require('aws-sdk');
+
+export const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    accessKeyId: '9cDM4HrHnD8cZUWf+6x7NQ==',
+    secretAccessKey: 'dMtK2Fw7xs0hUXRWq+pvKkpp1rYDWyozTlWR2TEd2JI=',
+    endpoint: 'https://storageapi.fleek.co',
+    region: 'us-east-1',
+    s3ForcePathStyle: true
+});
